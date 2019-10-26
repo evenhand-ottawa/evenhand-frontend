187f92e14cd22bfb9fa3ec56c15549aca85114c1
neil.mispelaar <neil.mispelaar@outlook.com>
2019-10-26 14:47:10 -0400

    Update Navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
+import Logo from '../images/logo/even_hand_transparent.png'
+
+console.log(Logo);
+
 
 export function NavBar() {
 	return (
 
-        <nav className="navbar navbar-dark bg-dark">
-            <div className="container">
+        <nav className="navbar py-3 py-md-5">
 
-                <h2 className="sr-only">EvenHand Navigation Menu</h2>
+            <h2 className="sr-only">EvenHand Navigation Menu</h2>
 
-                <a className="navbar-brand" href="#">EvenHand</a>
+            <a className="navbar-brand" href="#">
+                <img src={Logo} width="175px" alt="Evenhand Logo" />
+            </a>
 
-                <form className="form-inline my-2 my-lg-0">
-                    <button type="button" class="btn btn-primary">Log in</button>
-                </form>
+            <form className="form-inline">
+                <button className="btn btn-success" type="button">I Am a Speaker</button>
+            </form>
 
-            </div>
         </nav>
 
     )
