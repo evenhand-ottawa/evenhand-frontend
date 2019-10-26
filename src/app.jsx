db15f4f4c058bd9df4f8ce55e407ca5aa2d3580f
Karim Alibhai <karim@alibhai.co>
2019-10-26 12:46:37 -0400

    Add: separate home

--- src/app.jsx
+++ src/app.jsx
@@ -6,13 +6,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
+import { Home } from './components/home'
 import { Search } from './components/search'
 
 function App() {
 	return (
 		<Router>
 			<Switch>
-				<Route exact path="/" component={Search} />
+				<Route exact path="/" component={Home} />
+				<Route exact path="/search" component={Search} />
 			</Switch>
 		</Router>
 	)
