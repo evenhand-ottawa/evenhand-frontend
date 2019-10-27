a1000195959d57e372e4144d86bb5c5c80badb50
neil.mispelaar <neil.mispelaar@outlook.com>
2019-10-27 10:35:53 -0400

    Update footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,16 +1,11 @@
 import React from 'react'
 
-var footerStyle = {
-    backgroundColor: 'white',
-    
-  };
-
 export function Footer() {
 	return (
 
         <footer>
-            <div style={footerStyle}>
-                <div className="container">
+            <div className="footer">
+                <div className="container mt-5 py-5">
                     <h2>EvenHand Footer</h2>
                 </div>
             </div>
